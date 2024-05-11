379e66923d5028a3d81e55d1ae6b67fa085742cb
Bhaanoo Vishwakarma <bhanoolohar007@gmail.com>
2024-05-11 18:01:29 +0530

    minor updates

--- index.js
+++ index.js
@@ -31,7 +31,7 @@
   
 // personIntro.call({ firstName : 'Harry', lastName : 'Potter' }); // Output 3: Harry Potter
 
-// Concatinate an array to another array
+Concatinate an array to another array
 let arr = ['Ram', 'Lakshman', 'Bharat' ]
 let arr1 = [1, 3 ,5, 7]
 // Just concatinate this array
@@ -39,3 +39,12 @@ console.log(arr.concat(arr1));
 
 const arr2 = arr.push.apply(arr1, arr)
 console.log(arr2);
+// Concatinate an array to another array
+// let arr = ['Ram', 'Lakshman', 'Bharat' ]
+// let arr1 = [1, 3 ,5, 7]
+// // Just concatinate this array
+// console.log(arr.concat(arr1)); 
+
+// const arr2 = arr.push.apply(arr1, arr)
+// console.log(arr2);
+
